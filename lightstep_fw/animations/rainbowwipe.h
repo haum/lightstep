dd395a2430474a6d6b4104c835badfbc7a870975
Romuald Conty <neomilium@gmail.com>
2015-12-13 22:39:55 +0100

    Remove unused code

--- lightstep_fw/animations/rainbowwipe.h
+++ lightstep_fw/animations/rainbowwipe.h
@@ -5,9 +5,6 @@
 
 #include "../utils/wheel.h"
 
-void rainbowwipe_up(CRGB *leds, int time);
-void rainbowwipe_ring(CRGB *leds, int time);
-void rainbowwipe_updown(CRGB *leds, int time);
 void my_rainbowwipe_updown(CRGB *leds, int time);
 
 #endif
