fbfc23ff4acdc4e33b3d2677138a16ad77e652e0
Romuald Conty <neomilium@gmail.com>
2015-12-13 19:10:17 +0100

    Clean some code
    Protect headers against multiple inclusion
    Move headers inclusion in implementation instead of headers when relevant

--- lightstep_fw/animations/changingled.h
+++ lightstep_fw/animations/changingled.h
@@ -1,5 +1,8 @@
-#include "../ledconfig.h"
+#ifndef __CHANGINGLED_H__
+#define __CHANGINGLED_H__
+
 #include <FastLED.h>
 
 void pulse(CRGB *leds, struct CRGB color, int led, uint8_t time);
 
+#endif
--- lightstep_fw/animations/rainbowwipe.h
+++ lightstep_fw/animations/rainbowwipe.h
@@ -1,8 +1,13 @@
-#include "../ledconfig.h"
-#include "../utils/wheel.h"
+#ifndef __RAINBOWWIPE_H__
+#define __RAINBOWWIPE_H__
+
 #include <FastLED.h>
+
+#include "../utils/wheel.h"
 
 void rainbowwipe_up(CRGB *leds, int time);
 void rainbowwipe_ring(CRGB *leds, int time);
 void rainbowwipe_updown(CRGB *leds, int time);
 void my_rainbowwipe_updown(CRGB *leds, int time);
+
+#endif
