ac4f16ecf24411a390aaffc5e99eb1280fa758de
JbLb <jerome@jblb.net>
2015-12-14 20:33:01 +0100

    make K2000 animation with prameters

--- lightstep_fw/animations/K2000.cpp
+++ lightstep_fw/animations/K2000.cpp
@@ -15,18 +15,18 @@ void AnimationK2000::animate(CRGB *leds, const CRGB baseColor, const uint8_t ste
 		time++;
 	}
 
-	int height = time%7;
+	int height = time%LINES;
 	int height_trace = height-1;
 
-	if (time/7%2==0) {
-		height = 6 - height;
-		height_trace = 6 - height_trace;
+	if (time/LINES%2==0) {
+		height = (LINES-1) - height;
+		height_trace = (LINES-1) - height_trace;
 	}
 
 	if (height_trace == -1) height_trace = 0;
-	if (height_trace == 7) height_trace = 6;
+	if (height_trace == LINES) height_trace = (LINES-1);
 
-	for (int j=0; j<4 ; j++) {
+	for (int j=0; j<COLUMNS ; j++) {
 		leds[ring_leds[height_trace][j]].setRGB(baseColor.r/(j+1), baseColor.g/(j+1), baseColor.b/(j+1));
 		leds[ring_leds[height][j]].setRGB(baseColor.r, baseColor.g, baseColor.b);
 	}
