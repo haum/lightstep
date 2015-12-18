e45468afad8123d200e07b39aa578dfdc070c2e9
Romuald Conty <neomilium@gmail.com>
2015-12-18 23:24:19 +0100

    luz.h: use defines instead of hardcoded values

--- lightstep_fw/luz.h
+++ lightstep_fw/luz.h
@@ -3,27 +3,30 @@
 
 #define LEDORDER GRB
 #define NLEDS 25
+
 #define COLUMNS 4
 #define LINES 6
-#define RINGS 7
-#define GARLAND_LENGTH 12
 
-//
+#define RINGS 7
+
+#define GARLAND_LENGTH	12
+#define GARLAND_COUNT		2
+
 // Mappings
 //  * Two garlands around the middle axe (from bottom-left to upper-right)
-static const uint8_t garland_leds[2][12] = {
+static const uint8_t garland_leds[GARLAND_COUNT][GARLAND_LENGTH] = {
     { 0, 3, 4, 7, 8, 11, 12, 15, 16, 19, 20, 23 },
     { 1, 2, 5, 6, 9, 10, 13, 14, 17, 18, 21, 22 }
 };
 //  * Four columns (Front, Right, Left, Back)
-static const uint8_t column_leds[4][6] = {
+static const uint8_t column_leds[COLUMNS][LINES] = {
     { 20, 17, 12, 9, 4, 1 },
     { 22, 19, 14, 11, 6, 3 },
     { 21, 16, 13, 8, 5, 0 },
     { 23, 18, 15, 10, 7, 2 }
 };
 //  * seven inclined rings (from bottom-left to upper-right)
-static const uint8_t ring_leds[7][4] = {
+static const uint8_t ring_leds[RINGS][4] = {
     { 0, 1, 3, 0},
     { 5, 4, 6, 2},
     { 8, 9, 11, 7},
