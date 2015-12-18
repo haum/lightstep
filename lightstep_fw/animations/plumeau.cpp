3ef0de98da92efae9dd5ca4cc52df36d1000695b
JackDesBwa <jackdesbwa@gmail.com>
2015-12-18 23:27:54 +0100

    rework plumeau to use baseColor

--- lightstep_fw/animations/plumeau.cpp
+++ lightstep_fw/animations/plumeau.cpp
@@ -9,19 +9,14 @@ AnimationPlumeau::AnimationPlumeau():
 }
 
 void AnimationPlumeau::animate(Framebuffer &leds, CHSV baseColor, const uint8_t step) {
-	prescaler_counter++;
-	if (prescaler_counter%10 == 0) {
-		prescaler_counter = 0;
-		time++;
-	}
-
-	int height = time%LINES;
-	if (time/LINES%2==0) {
-		height = (LINES-1) - height;
-	}
-	for (int i=0; i <= height; i++) {
-		for (int j=0; j<COLUMNS ; j++) {
-			leds[column_leds[j][i]]= CHSV( ( (i * 256 / LINES ) % 255), 255, 255);
+	uint8_t sup = map(triwave8(step), 0, 255, 0, LINES + 1);
+	if (sup > 0)
+		leds[24] = baseColor;
+	for (uint8_t i = 0; i < sup; ++i) {
+		uint8_t line = i >= LINES ? LINES - 1 : i;
+		for (uint8_t j = 0; j < COLUMNS; ++j) {
+			leds[column_leds[j][i]] = baseColor;
 		}
+		baseColor.hue += 255 / LINES;
 	}
 }
